d3373240e56cf0e9882093156cbb9879f63275f4
Knowther <johnnywesley.contato@outlook.com>
2022-11-24 15:01:22 -0300

    app tip finished

--- TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val myButton: Button = binding.calculateButton
+
+        myButton.setOnClickListener {
+            calculateTip()
+        }
+    }
+
+    private fun calculateTip(){
+        val  stringTextinField = binding.costOfService.text.toString()
+        val cost = stringTextinField.toDoubleOrNull()
+        if (cost == null){
+            binding.tipResult.text = "Insert a value on tip field"
+            return
+        }
+
+        val tipPercentage = when(binding.buttonGroup.checkedRadioButtonId){
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        var tip = tipPercentage * cost
+        if (binding.roundUpSwitch.isChecked) tip = kotlin.math.ceil(tip)
+       val formatedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formatedTip)
     }
 }
